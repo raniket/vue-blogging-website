70b2b2bbb73a1a5fb8df02ff7213cef8fcb31ad2
raniket <raniketr@selotsoft.com>
2018-02-26 18:58:33 +0530

    initialized all files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+
+    },
+    data: function() {
+
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
